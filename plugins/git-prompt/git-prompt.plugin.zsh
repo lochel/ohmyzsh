b58ef14f43edeea0eb5d6b200685a2d4fd726095
Lennart Ochel <lennart.ochel@vti.se>
2022-11-12 12:32:54 +0100

    Show separator only if there is something to separate

--- plugins/git-prompt/git-prompt.plugin.zsh
+++ plugins/git-prompt/git-prompt.plugin.zsh
@@ -59,54 +59,59 @@ function update_current_git_vars() {
 git_super_status() {
     precmd_update_git_vars
     if [ -n "$__CURRENT_GIT_STATUS" ]; then
-      STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH$GIT_UPSTREAM%{${reset_color}%}"
+      STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH$GIT_UPSTREAM"
       if [ "$GIT_BEHIND" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
+          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND"
       fi
       if [ "$GIT_AHEAD" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_AHEAD$GIT_AHEAD%{${reset_color}%}"
+          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_AHEAD$GIT_AHEAD"
       fi
-      STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
       if [ "$GIT_STAGED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
+          STATUS2="$STATUS2$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED"
       fi
       if [ "$GIT_CONFLICTS" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
+          STATUS2="$STATUS2$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS"
       fi
       if [ "$GIT_CHANGED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
+          STATUS2="$STATUS2$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED"
       fi
       if [ "$GIT_DELETED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_DELETED$GIT_DELETED%{${reset_color}%}"
+          STATUS2="$STATUS2$ZSH_THEME_GIT_PROMPT_DELETED$GIT_DELETED"
+      fi
+      if [ "$GIT_DELETED" -ne "0" ]; then
+          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_DELETED$GIT_DELETED"
       fi
       if [ "$GIT_UNTRACKED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED$GIT_UNTRACKED%{${reset_color}%}"
+          STATUS2="$STATUS2$ZSH_THEME_GIT_PROMPT_UNTRACKED$GIT_UNTRACKED"
       fi
       if [ "$GIT_STASHED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STASHED$GIT_STASHED%{${reset_color}%}"
+          STATUS2="$STATUS2$ZSH_THEME_GIT_PROMPT_STASHED$GIT_STASHED"
       fi
       if [ "$GIT_CLEAN" -eq "1" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
+          STATUS2="$STATUS2$ZSH_THEME_GIT_PROMPT_CLEAN"
       fi
-      STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+      if [ ! -z "$STATUS2" ]; then
+          STATUS2=$ZSH_THEME_GIT_PROMPT_SEPARATOR$STATUS2
+      fi
+      STATUS="$STATUS$STATUS2$ZSH_THEME_GIT_PROMPT_SUFFIX"
       echo "$STATUS"
     fi
 }
 
 # Default values for the appearance of the prompt.
-ZSH_THEME_GIT_PROMPT_PREFIX="("
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"
-ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
-ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}%{●%G%}"
-ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{✖%G%}"
-ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}%{✚%G%}"
-ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[blue]%}%{-%G%}"
-ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}%{…%G%}"
-ZSH_THEME_GIT_PROMPT_STASHED="%{$fg_bold[blue]%}%{⚑%G%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{✔%G%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="(%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX=")%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_SEPARATOR="|%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}%{●%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{✖%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}%{✚%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[blue]%}%{-%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}%{…%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_STASHED="%{$fg_bold[blue]%}%{⚑%G%}%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{✔%G%}%{${reset_color}%}"
 ZSH_THEME_GIT_PROMPT_UPSTREAM_SEPARATOR="->"
 
 # Set the prompt.
